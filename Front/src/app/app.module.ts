6f6d9322c9da860da8d00e4e372d11939104be51
Daniel <danielesteban2004@gmail.com>
2022-06-02 15:21:48 -0500

    se arregla el fondo

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,24 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { FeedComponent } from './feed/feed.component';
-import { CreateComponent } from './create/create.component';
-=======
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
->>>>>>> 9723205d826f062d946c7b31fa02b73c79d2862f
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    FeedComponent,
-    CreateComponent
-=======
     NavBarComponent,
     HomeComponent
->>>>>>> 9723205d826f062d946c7b31fa02b73c79d2862f
   ],
   imports: [
     BrowserModule,

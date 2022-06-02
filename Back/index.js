836320d05facce13a69b110f8158429864733ccd
Daniel <danielesteban2004@gmail.com>
2022-06-01 19:51:37 -0500

    Se agregó lo básico del bak

---
+++ Back/index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+require("dotenv").config()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.LINK)
+    .then((db) => { console.log("Se ha conectado a la BD") })
+    .catch((err) => { console.log(err) });
+
+
+app.listen(3000);

15a6694ed349bd5b4fa3849221fc0d887e4ad76d
Aaron Alicastro <aalicastro5@gmail.com>
2022-06-06 18:23:23 -0500

    agregadas las rutas del registro, el inicio de sesion y del chat. tambien se agrego la funcionalidad del registro y del inicio

--- Front/src/app/registro/registro.component.ts
+++ Front/src/app/registro/registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { perfiles } from '../Models/perfiles';
+import { PublicacionesService } from '../Services/publicaciones-service.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  perfil: perfiles = {
+    userName: "",
+    userDescripcion: undefined,
+    urlImage: undefined,
+    email: "",
+    contrasena: ""
+  }
 
-  constructor() { }
+  constructor(private servidor: PublicacionesService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  sendPerfil(form: NgForm){
+    this.servidor.createPerfil(form.value).subscribe({next: (r:any) => {
+      alert(r.response);
+      this.router.navigate(['feed']);
+    },
+    error: e => {
+      console.log(e);
+    }});
+  }
 }
